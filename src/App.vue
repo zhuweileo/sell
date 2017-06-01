a3a8047a431c6f7f0ea3c203f7315e9dac57b0a8
zhuwei <zhuwei_leo@163.com>
2017-06-01 23:07:44 +0800

    新建header组件

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,19 @@
 <template>
   <div id="app">
+      我是app
+  <v-header></v-header>
   </div>
 </template>
 
 <script>
+import header from './components/header/header.vue';
 export default {
+  components: {
+    'v-header': header
+  }
 };
 </script>
 
 <style>
-html {
-  height: 100%;
-}
 
-body {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  height: 100%;
-}
-
-#app {
-  color: #2c3e50;
-  margin-top: -100px;
-  max-width: 600px;
-  font-family: Source Sans Pro, Helvetica, sans-serif;
-  text-align: center;
-}
-
-#app a {
-  color: #42b983;
-  text-decoration: none;
-}
-
-.logo {
-  width: 100px;
-  height: 100px
-}
 </style>
