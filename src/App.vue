6cf5c02a6ec02d39b1c333d6eecf8a9126036a9d
zhuwei <zhuwei_leo@163.com>
2017-06-04 20:36:03 +0800

    添加vue-resource

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <v-header></v-header>
-    <div class="tab">
+    <div class="tab border-1px">
       <div class="tab-item">
         <a v-link="{path:'/goods'}">商品</a>
       </div>
@@ -9,7 +9,7 @@
         <a v-link="{path:'/ratings'}">评论</a>
       </div>
       <div class="tab-item">
-        <a v-link="{path:'/seller'}">商家</a>
+        <a v-link="{path:'/sellers'}">商家</a>
       </div>
     </div>
     <router-view></router-view>
@@ -26,12 +26,24 @@ export default {
 </script>
 
 <style lang="scss">
-#app{
-  .tab{
-    display: flex;
-    .tab-item{
-      flex-grow: 1;
-      text-align:center;
+@import "../common/scss/mixin.scss";
+.tab {
+  display: flex;
+  width:100%;
+  height: 40px;
+  line-height: 40px;
+  // border-bottom: 1px solid rgba(7,17,27,0.1);
+  @include border-1px(rgba(7,17,27,0.1));
+  .tab-item {
+    flex-grow: 1;
+    text-align: center;
+    a{
+      display: block;
+      font-size: 14px;
+      color: rgb(77, 85, 93);
+      &.active{
+        color:rgb(240, 20, 20);
+      }
     }
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,18 +1,29 @@
 import Vue from 'vue';
 import App from './App';
 import VueRouter from 'vue-router';
-import goods from './components/goods/goods';
+import goods from 'components/goods/goods';
+import ratings from 'components/ratings/ratings';
+import sellers from 'components/sellers/sellers';
 
 Vue.use(VueRouter);
 
 let app = Vue.extend(App);
 
-let router = new VueRouter();
+let router = new VueRouter({
+  linkActiveClass: 'active'
+});
 
 router.map({
   '/goods': {
     component: goods
+  },
+  '/ratings': {
+    component: ratings
+  },
+  '/sellers': {
+    component: sellers
   }
 });
 
 router.start(app, '#app');
+router.go('/goods');
