3fcd813a7be5ea813ab63807bf5b68bae9eede0e
zhuweileo <zhuwei_leo@163.com>
2017-08-20 16:39:05 +0800

    goods组件布局

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -1,11 +1,46 @@
 <template>
-<div>goods</div>
+    <div class="goods">
+        <div class="menu-wrapper"></div>
+        <div class="foods-wrapper"></div>
+    </div>
 </template>
 <script type="text/ecmascript-6">
-export default{
+const OK = 0;
+export default {
+    props: {
+        seller:Object
+    },
+    data(){
+        return {
+            goods:[]
+        }
+    },
+    created(){
+        this.$http.get('/api/goods').then((response) => {
+            response = response.body;
+            if(response.errno === OK){
+                this.goods = response.data;
+            }
+        })
+    }
 
 };
 </script>
 
-<style lang="">
+<style lang="scss">
+.goods {
+    display: flex;
+    position: absolute;
+    top: 174px;
+    bottom: 46px;
+    width: 100%;
+    .menu-wrapper {
+        flex: 0 0 80px;
+        background: #f3f5f7;
+        width: 80px;
+    }
+    .foods-wrapper {
+        flex: 1;
+    }
+}
 </style>
